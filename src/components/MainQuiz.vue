5a73ca2c59f234655445c879559c24c30439cf60
Johan Åström <johan.astrom89@gmail.com>
2021-05-07 10:51:45 +0200

    Added MainQuiz component, toggled view

---
+++ src/components/MainQuiz.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+  <h1>Quiz!!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MainQuiz"
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,23 +1,28 @@
 <template>
-<section>
-  <QuizSettings @showQuiz="displayQuiz" v-if="!showQuiz"/>
-</section>
+  <section>
+    <QuizSettings @showQuiz="displayQuiz" v-if="!showQuiz" />
+    <MainQuiz v-if="showQuiz"/>
+  </section>
 </template>
 
 <script>
 import QuizSettings from "@/components/QuizSettings";
+import MainQuiz from "@/components/MainQuiz";
 
 export default {
   name: "Quiz",
-  components: { QuizSettings },
-  data(){
-    return{
+  components: {
+    QuizSettings,
+    MainQuiz
+  },
+  data() {
+    return {
       showQuiz: false
-    }
+    };
   },
   methods: {
     displayQuiz() {
-      this.showQuiz=true;
+      this.showQuiz = true;
     }
   }
 };
