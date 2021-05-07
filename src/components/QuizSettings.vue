3dc2cb4d4abe59b89c6dbeca1bb4beba0778677b
Johan Åström <johan.astrom89@gmail.com>
2021-05-07 11:10:37 +0200

    Some styling for QuizSettings

--- src/components/QuizSettings.vue
+++ src/components/QuizSettings.vue
@@ -1,19 +1,19 @@
 <template>
   <section>
-    <article>
-      <b-dropdown text="Addition" @show="setOperator('addition')">
+    <article id="dropdowns">
+      <b-dropdown class="dropdown" text="Addition" @show="setOperator('addition')">
         <b-dropdown-item @click="setDifficulty('easy')">Lätt</b-dropdown-item>
         <b-dropdown-item @click="setDifficulty('hard')">Svårt</b-dropdown-item>
       </b-dropdown>
-      <b-dropdown text="Subtraktion" @show="setOperator('subtraction')">
+      <b-dropdown class="dropdown" text="Subtraktion" @show="setOperator('subtraction')">
         <b-dropdown-item @click="setDifficulty('easy')">Lätt</b-dropdown-item>
         <b-dropdown-item @click="setDifficulty('hard')">Svårt</b-dropdown-item>
       </b-dropdown>
-      <b-dropdown text="Division" @show="setOperator('division')">
+      <b-dropdown class="dropdown" text="Division" @show="setOperator('division')">
         <b-dropdown-item @click="setDifficulty('easy')">Lätt</b-dropdown-item>
         <b-dropdown-item @click="setDifficulty('hard')">Svårt</b-dropdown-item>
       </b-dropdown>
-      <b-dropdown text="Multiplikation" @show="setOperator('multiplication')">
+      <b-dropdown class="dropdown" text="Multiplikation" @show="setOperator('multiplication')">
         <b-dropdown-item @click="setDifficulty('easy')">Lätt</b-dropdown-item>
         <b-dropdown-item @click="setDifficulty('hard')">Svårt</b-dropdown-item>
       </b-dropdown>
@@ -45,4 +45,18 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+#dropdowns {
+  display: flex;
+  flex-direction: column;
+}
+
+.dropdown{
+  width: 150px;
+  align-self: center;
+  margin-bottom: 15px;
+}
+
+
+</style>
