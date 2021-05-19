f86c01937b7a977940ea14e9adfa22070cff3bd2
Johan Åström <johan.astrom89@gmail.com>
2021-05-19 11:07:49 +0200

    Added SignupPage component with form

---
+++ src/components/SignupPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2>Registrera användare</h2>
+    <form id="signup-form" method="POST">
+      <label for="username">Ange ett användarnamn:</label>
+      <input id="username" name="username" />
+      <label for="password">Ange ett lösenord:</label>
+      <input id="password" name="password" />
+      <input type="submit" value="Skicka" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SignupPage"
+};
+</script>
+
+<style scoped>
+
+</style>
