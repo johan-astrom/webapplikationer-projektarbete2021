c4088e8c4874910fbf26f31a308c29eefadb2045
unknown <nik.ary94@gmail.com>
2021-05-26 11:05:17 +0200

    fixat ändra lösenord

--- src/views/ProfileInfo.vue
+++ src/views/ProfileInfo.vue
@@ -1,23 +1,75 @@
 <template>
   <body>
   <section class="grid_container">
-      <div class="profile_container">
-        <b-card class="card" no-body>
-          <b-tabs pills card vertical>
-            <b-tab title="Visa Resultat" active><b-card-text>Tab contents 1</b-card-text></b-tab>
-            <b-tab title="Ändra Lösenord"><b-card-text>Tab contents 2</b-card-text></b-tab>
-            <b-tab title="Ta bort Konto"><b-card-text>Tab contents 3</b-card-text></b-tab>
-            <b-tab v-on:click="logout" title="Logga Ut"><b-card-text>Logga ut</b-card-text></b-tab>
-          </b-tabs>
-        </b-card>
-      </div>
+    <div class="profile_container">
+      <b-card class="card" no-body>
+        <b-tabs pills card vertical>
+          <b-tab title="Visa Resultat" active>
+            <b-card-text>Tab contents 1</b-card-text>
+          </b-tab>
+
+            <b-tab title="Ändra Lösenord">
+              <b-card-text>
+                <div id="Ändra lösenord" class="tabcontent">
+                  <h3>Byt Lösenord</h3>
+                  <form id="changePassword-form" method="POST" @submit.prevent="checkUsername()">
+                    <label for="password">Ange ditt nya lösenord:</label>
+                    <br>
+                    <input class="signup_password_container"
+                           type="password"
+                           id="password"
+                           name="password"
+                           v-model="password"
+                           minlength="6"
+                           maxlength="15"
+                           required
+                    />
+                    <br>
+                    <label for="password-check">Bekräfta ditt lösenord:</label>
+                    <br>
+                    <input
+                        type="password"
+                        id="password-check"
+                        name="password-check"
+                        v-model="passwordCheck"
+                        minlength="6"
+                        maxlength="15"
+                        required
+                    />
+                    <p v-if="passwordCheck!==password && passwordCheck">Lösenorden stämmer inte överens!</p>
+                    <br>
+                    <br>
+                    <input type="submit" v-show="passwordCheck===password" value="Skicka"/>
+                  </form>
+                </div>
+              </b-card-text>
+            </b-tab>
+
+            <b-tab title="Ta bort Konto">
+              <b-card-text>Tab contents 3</b-card-text>
+            </b-tab>
+            <b-tab v-on:click="logout" title="Logga Ut">
+              <b-card-text>Logga ut</b-card-text>
+            </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
   </section>
   </body>
 </template>
 
 <script>
 export default {
+
   name: "ProfileInfo",
+  data() {
+    return {
+      username: "",
+      password: "",
+      passwordCheck: "",
+      postUrl: "http://localhost:3000/users/"
+    }
+  },
   props: {
     loggedIn: Boolean
   },
@@ -27,21 +79,54 @@ export default {
     },
     changePassword() {
 
+
     },
     deleteAccount() {
 
     },
     logout() {
       this.$emit("user-login-step3");
+    },
+    checkUsername() {
+      if (
+          this.username.toLowerCase().includes("fuck") ||
+          this.username.toLowerCase().includes("snopp") ||
+          this.username.toLowerCase().includes("fan")
+      ) {
+        alert("Användarnamnet innehåller fula ord!");
+      } else {
+        this.postData(this.postUrl);
+      }
+    },
+    postData: async function (url = "") {
+      const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password
+        })
+      }).then((response) => {
+        if (response.status === 400) {
+          alert("Användarnamnet är upptaget.");
+        } else {
+          return response.json();
+        }
+      });
     }
-   }
+  }
 }
 </script>
 
 <style scoped>
-.grid_container{
+.grid_container {
   display: grid;
-  grid-template-columns: 1fr ;
+  grid-template-columns: 1fr;
   grid-template-rows: 2.0fr;
   grid-template-areas:
     "header header "
